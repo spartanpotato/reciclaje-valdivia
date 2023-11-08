825692890ba518d177f48a4f7151e0232f6649ab
MaDummy <alvaro.burgosandrade@gmail.com>
2023-11-08 10:38:28 -0300

    Cambiado Header.js pa que los q no entienden como funciona la pag..

--- app/components/Header.js
+++ app/components/Header.js
@@ -17,6 +17,12 @@ export default function Header() {
     <nav className="navegacion">
         <img src="reciclaje.png" width="6%" height="5%"/> 
         <p className="titulo">Puntos de reciclaje en Valdivia</p>
+        <div className="leyenda">
+          <ul>
+            <li>Obtenga mas informacion presionando los marcadores</li>
+            <li>Use los filtros para que solo aparezcan puntos con ese tipo de basura</li>
+          </ul>
+        </div>
     </nav>
   );
 }
