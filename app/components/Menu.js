3c39341ebfd078359764ec213b4a6f2f947e369e
MaDummy <alvaro.burgosandrade@gmail.com>
2023-11-28 23:41:58 -0300

    Decorado menu filtros

--- app/components/Menu.js
+++ app/components/Menu.js
@@ -40,23 +40,23 @@ function Menu_1( {data} ) {
 
             <MenuList className="Menu" closeOnSelect="false">
 
-                <MenuItem className="Checkbox">
+                <MenuItem className="checkboxVidrios">
                     <Checkbox  onChange={()=>setVidrio(!Vidrio)}>Vidrio</Checkbox>
                 </MenuItem>
 
-                <MenuItem className="Checkbox">
+                <MenuItem className="checkboxLatas">
                     <Checkbox  onChange={()=>setLatas(!Latas)}>Latas</Checkbox>
                 </MenuItem>
 
-                <MenuItem className="Checkbox">
+                <MenuItem className="checkboxPlasticos">
                     <Checkbox  onChange={()=>setPlastico(!Plastico)}>Plastico</Checkbox>
                 </MenuItem>
 
-                <MenuItem className="Checkbox">
+                <MenuItem className="checkboxCarton">
                     <Checkbox onChange={()=>setPapel_Carton(!Papel_Carton)}>Papel/Carton</Checkbox>
                 </MenuItem>
 
-                <MenuItem className="Checkbox">
+                <MenuItem className="checkboxOrganico">
                     <Checkbox  onChange={()=>setOrganico(!Organico)}>Organico</Checkbox>
                 </MenuItem>
 
