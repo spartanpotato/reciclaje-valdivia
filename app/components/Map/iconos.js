b3c74818f7edbd1c5634be296b7d812578b5f15c
spartanpotato <107315314+spartanpotato@users.noreply.github.com>
2024-12-15 23:03:22 -0300

    arreglos a medias iconos y drawer

--- app/components/Map/iconos.js
+++ app/components/Map/iconos.js
@@ -1,6 +1,6 @@
 export default function Icons(tipos) {
-    console.log("Received tipos:", tipos); // Debug: Log the input data
-    var ImgURL = "./Icon/";
+    console.log("llego a iconos.js: ", tipos);
+    var ImgURL = "/Icon/";
     var img = "";
     var contador = 0;
     const ids = ["o_", "l_", "pc_", "v_", "p_"];
@@ -12,9 +12,7 @@ export default function Icons(tipos) {
         contador += 1;
     });
 
-    console.log("Constructed img string:", img); // Debug: Log the constructed string
     img = img.slice(0, -1); // Remove trailing underscore
     ImgURL += img + ".png";
-    console.log("Final ImgURL:", ImgURL); // Debug: Log the final URL
     return ImgURL;
 }
